bc5d964e81c90c1384d5cd72838f5b13856aa490
ChenXiaoyao <chenxiaoyao6228@163.com>
2019-12-03 13:19:19 +0800

    refactor(facade): remove unecessary renderer obj

--- packages/facade/src/renderer.js
+++
@@ -1,7 +0,0 @@
-export let renderer = {
-  nextUnitOfWork: null, //  需要处理的下一个fiber对象, 浏览器空闲的时候会处理
-  wipRoot: null, // 本次更新的wip, 根节点
-  wipFiber: null, // 正在被处理的fiber节点
-  currentRoot: null, // commit阶段被赋值,下次更新的alternate
-  deletions: []
-};
